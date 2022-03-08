fdf4427c4eaf4581dc1658456f3964a3c0a6a342
ChewKinWhye <Ckinwhye@hotmail.com>
2022-03-08 17:39:45 +0800

    Contrastive Learning

---
+++ contrastive_learning.py
@@ -0,0 +1,148 @@
+import numpy as np
+import torch
+import torchvision
+from torch.utils.data import Dataset, DataLoader
+from torchvision import datasets
+from PIL import Image
+import torch.nn as nn
+from sklearn.manifold import TSNE
+from matplotlib import pyplot as plt
+
+
+class NT_Xent(nn.Module):
+    def __init__(self, batch_size, temperature, world_size):
+        super(NT_Xent, self).__init__()
+        self.batch_size = batch_size
+        self.temperature = temperature
+        self.world_size = world_size
+
+        self.mask = self.mask_correlated_samples(batch_size, world_size)
+        self.criterion = nn.CrossEntropyLoss(reduction="sum")
+        self.similarity_f = nn.CosineSimilarity(dim=2)
+
+    def mask_correlated_samples(self, batch_size, world_size):
+        N = 2 * batch_size * world_size
+        mask = torch.ones((N, N), dtype=bool)
+        mask = mask.fill_diagonal_(0)
+        for i in range(batch_size * world_size):
+            mask[i, batch_size * world_size + i] = 0
+            mask[batch_size * world_size + i, i] = 0
+        return mask
+
+    def forward(self, z_i, z_j):
+        """
+        We do not sample negative examples explicitly.
+        Instead, given a positive pair, similar to (Chen et al., 2017), we treat the other 2(N − 1) augmented examples within a minibatch as negative examples.
+        """
+        N = 2 * self.batch_size * self.world_size
+
+        z = torch.cat((z_i, z_j), dim=0)
+
+        sim = self.similarity_f(z.unsqueeze(1), z.unsqueeze(0)) / self.temperature
+
+        sim_i_j = torch.diag(sim, self.batch_size * self.world_size)
+        sim_j_i = torch.diag(sim, -self.batch_size * self.world_size)
+
+        # We have 2N samples, but with Distributed training every GPU gets N examples too, resulting in: 2xNxN
+        positive_samples = torch.cat((sim_i_j, sim_j_i), dim=0).reshape(N, 1)
+        negative_samples = sim[self.mask].reshape(N, -1)
+
+        labels = torch.zeros(N).to(positive_samples.device).long()
+        logits = torch.cat((positive_samples, negative_samples), dim=1)
+        loss = self.criterion(logits, labels)
+        loss /= N
+        return loss
+
+def triplet_contrastive_loss(x, B_pos, B_neg):
+    loss = -torch.mean(torch.matmul(B_pos, x)) + torch.mean(torch.matmul(B_neg, x))
+    return loss
+
+
+class SelfconLoss(nn.Module):
+    def __init__(self):
+        super(SelfconLoss, self).__init__()
+    def forward(self, z_i, z_j):
+        loss = 0
+        n = len(z_i)
+        for index, (d_i, d_j) in enumerate(zip(z_i, z_j)):
+            loss += 2 * torch.dot(d_i, d_j)
+            mask = torch.ones(n)
+            mask[index] = 0
+            loss -= torch.dot(torch.matmul(z_i, d_i), mask)/(2 * n - 2)
+            loss -= torch.dot(torch.matmul(z_i, d_j), mask)/(2 * n - 2)
+            loss -= torch.dot(torch.matmul(z_j, d_i), mask)/(2 * n - 2)
+            loss -= torch.dot(torch.matmul(z_j, d_j), mask)/(2 * n - 2)
+        loss = -loss / (2 * n)
+        return loss
+
+def regularization_loss(model):
+    print(model.linear.weight.size())
+
+class DataHandler(Dataset):
+    def generate_random_mask(self, size):
+        random_mask = np.diag(np.rint(np.random.rand(size)))
+        return torch.tensor(random_mask)
+
+    def __init__(self, X):
+        self.X = X
+        self.random_mask = self.generate_random_mask(X.size()[1])
+
+    def __getitem__(self, index):
+        x = self.X[index]
+        x1 = torch.matmul(self.random_mask, x)
+        x2 = torch.matmul((1 - self.random_mask), x)
+        return x1, x2
+
+    def __len__(self):
+        return len(self.X)
+
+def train(train_loader, model, criterion, optimizer):
+    loss_epoch = 0
+    for step, (x_i, x_j) in enumerate(train_loader):
+        optimizer.zero_grad()
+        x_i = x_i.cuda(non_blocking=True)
+        x_j = x_j.cuda(non_blocking=True)
+
+        # positive pair, with encoding
+        z_i = model(x_i)
+        z_j = model(x_j)
+
+        loss = criterion(z_i, z_j)
+        loss.backward()
+
+        optimizer.step()
+        if step % 50 == 0:
+            print(f"Step [{step}/{len(train_loader)}]\t Loss: {loss.item()}")
+
+        loss_epoch += loss.item()
+    return loss_epoch
+
+
+class linear_CL_Model(torch.nn.Module):
+    def __init__(self, d, r):
+        super(linear_CL_Model, self).__init__()
+        self.linear = torch.nn.Linear(d, r, bias=False)
+
+    def forward(self, x):
+        out = self.linear(x)
+        return out
+
+def contrastive_training(r, d, x, loss_fn, batch_size=32, num_epochs=10, lr=3e-4):
+    # Data Loader
+    x = torch.tensor(x)
+    print(f"Train Data Shape: {x.size()}")
+    train_loader = DataLoader(DataHandler(x), shuffle=True, batch_size=batch_size, drop_last=True)
+    # Model
+    model = linear_CL_Model(d, r).double().cuda()
+    print(f"Model Size: {model.linear.weight.size()}")
+    # Optimizer
+    optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+    if loss_fn == "NTXENT":
+        criterion = NT_Xent(batch_size, 0.5, 1)
+    else:
+        criterion = SelfconLoss()
+    for epoch in range(num_epochs):
+        loss_epoch = train(train_loader, model, criterion, optimizer)
+        print(f"Epoch [{epoch}/{num_epochs}]\t Loss: {loss_epoch / len(train_loader)}\t")
+    return model
+
--- main.py
+++ main.py
@@ -3,16 +3,17 @@ import os
 import numpy as np
 import random
 import torch
-
+from contrastive_learning import contrastive_training
 from data_generator import SpikedCovarianceDataset
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--mode", default="train", help="Set Experiment Mode")
-parser.add_argument("--model", default="ae", help="Set Experiment Model")
+parser.add_argument("--model", default="cl", help="Set Experiment Model")
 parser.add_argument("--ckptfldr", default="v0", help="Folder for Saving Files")
 parser.add_argument("--cuda", action="store_true", help="Use CUDA")
 parser.add_argument("--gpus", default="0,1", help="GPU Device ID to use separated by commas")
-parser.add_argument("--seed", default="0", help="Random seed to allow replication of results")
+parser.add_argument("--seed", default=0, help="Random seed to allow replication of results")
+parser.add_argument("--num_unsup_datapoints", type=int, default=1000, help="Number of data points of unsupervised learning")
 
 ## Parameters for Data Generation
 parser.add_argument("--r", type=int, default=10, help="Representation Dimension of Original Signal")
@@ -21,8 +22,8 @@ parser.add_argument("--sigma", type=float, default=1.0, help="Standard Deviation
 
 ## Parameters for Training
 ## Add more and change as required
-parser.add_argument("--lr", type=float, default=0.001, help="Learning rate")
-parser.add_argument("--steps", type=int, default=100, help="Number of Steps for Training")
+parser.add_argument("--lr", type=float, default=3e-4, help="Learning rate")
+parser.add_argument("--steps", type=int, default=50000, help="Number of Steps for Training")
 
 args = parser.parse_args()
 
@@ -34,20 +35,25 @@ torch.manual_seed(args.seed)
 if args.mode=='train':
     # Train Model
     ## Call data generator
-
+    generator = SpikedCovarianceDataset(args.r, args.d, args.sigma, label_mode='classification')
+    X, y = generator.get_next_batch(batch_size=args.num_unsup_datapoints)
     if args.model=='ae':
         ## Call autoencoder trainer
+        pass
     elif args.model=='cl':
         ## Call contrastive learning trainer
-
+        model = contrastive_training(args.r, args.d, X, loss_fn="NTXENT",
+                                     batch_size=32, num_epochs=args.steps, lr=args.lr)
 elif args.mode=='test':
     # Test performances
     ## Call test data generator
 
     if args.model=='ae':
         ## Call appropriate parameter and model extractor
+        pass
     elif args.model=='cl':
         ## Call appropriate parameter and model extractor
+        pass
 
     ## Test matrix U-star
 
