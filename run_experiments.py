9c21a767207465792170c0f4a93cb42da3c0a4c3
Kshitij Singh <kanata@Kshitijs-MacBook-Pro.local>
2022-03-25 18:25:11 +0800

    main.py

--- run_experiments.py
+++ run_experiments.py
@@ -1,22 +1,32 @@
 from main import run, obtain_args
 import argparse
 import csv
+import math
 
+def cl_theoretical_upper_bound(d,r,n):
+    return ((r**1.5/d)*math.log(d)) + math.sqrt(d*r/n)
 
 def run_all_exp(args):
     # args.experiment = "increase_noise"
     # args.experiment = "increase_noise_homogenous"
     # args.experiment = "increase_dimension_d"
     # args.experiment = "increase_dimension_r"
-    args.experiment = "increase_dimension_r_model"
+    # args.experiment = "increase_dimension_r_model"
     # args.experiment = "increase_dimension_d_and_r"
     # args.experiment = "increase_n"
+    # args.experiment = "bound_cl_with_d"
+    args.experiment = "bound_cl_with_r"
+    # args.experiment = "bound_cl_with_n"
+    bound_experiments = ["bound_cl_with_d", "bound_cl_with_r", "bound_cl_with_n"]
 
     sinedistance_scores = {}
     downstream_scores = {}
+    
+    sinedistance_upper_bounds = {}
     for model in ["ae", "cl"]:
         sinedistance_scores[model] = [model]
         downstream_scores[model] = [model]
+        sinedistance_upper_bounds[model] = [model]
 
         if args.experiment == "increase_dimension_d":
             dimensions = ["Dimension", 20, 30, 40, 50, 60, 70, 80, 90, 100, 110]
@@ -59,6 +69,34 @@ def run_all_exp(args):
                 sinedistance_score, score = run(args)
                 sinedistance_scores[model].append(sinedistance_score)
                 downstream_scores[model].append(score)
+        if args.experiment == "bound_cl_with_d":
+            dimensions = ["Dimension", 20, 30, 40, 50, 60, 70, 80, 90, 100, 110]
+            for dimension in dimensions[1:]:
+                if model == "ae": break
+                args.model = model
+                args.d = dimension
+                sinedistance_score, _ = run(args)
+                sinedistance_upper_bound = cl_theoretical_upper_bound(args.d, args.r_model, args.train_size)
+                sinedistance_scores[model].append(sinedistance_score)
+                sinedistance_upper_bounds[model].append(sinedistance_upper_bound)
+        if args.experiment == "bound_cl_with_r":
+            dimensions = ["Model Representation", 2, 5, 10, 20, 40]
+            for dimension in dimensions[1:]:
+                args.model = model
+                args.r_model = dimension
+                sinedistance_score, _ = run(args)
+                sinedistance_upper_bound = cl_theoretical_upper_bound(args.d, args.r_model, args.train_size)
+                sinedistance_scores[model].append(sinedistance_score)
+                sinedistance_upper_bounds[model].append(sinedistance_upper_bound)
+        if args.experiment == "bound_cl_with_n":
+            n_values = ["n", 1000, 2000, 3000, 4000, 5000, 10000, 20000, 30000, 40000, 50000]
+            for n in n_values[1:]:
+                args.model = model
+                args.train_size = n
+                sinedistance_score, _ = run(args)
+                sinedistance_upper_bound = cl_theoretical_upper_bound(args.d, args.r_model, args.train_size)
+                sinedistance_scores[model].append(sinedistance_score)
+                sinedistance_upper_bounds[model].append(sinedistance_upper_bound)
     with open(f"{args.ckptfldr}/{args.experiment}_sinedistance_{args.seed}.csv", "w+", newline='') as f:
         writer = csv.writer(f)
 
@@ -72,27 +110,36 @@ def run_all_exp(args):
             writer.writerow(noise_sigmas)
         if args.experiment == "increase_noise_homogenous":
             writer.writerow(noise_sigmas)
-        writer.writerow(sinedistance_scores["ae"])
-        writer.writerow(sinedistance_scores["cl"])
-    with open(f"{args.ckptfldr}/{args.experiment}_downstream_score_{args.seed}.csv", "w+", newline='') as f:
-        writer = csv.writer(f)
-        if args.experiment == "increase_dimension_d":
+        if args.experiment == "bound_cl_with_d":
             writer.writerow(dimensions)
-        if args.experiment == "increase_dimension_r_model":
+        if args.experiment == "bound_cl_with_r":
             writer.writerow(dimensions)
-        if args.experiment == "increase_n":
+        if args.experiment == "bound_cl_with_n":
             writer.writerow(n_values)
-        if args.experiment == "increase_noise":
-            writer.writerow(noise_sigmas)
-        if args.experiment == "increase_noise_homogenous":
-            writer.writerow(noise_sigmas)
-        writer.writerow(downstream_scores["ae"])
-        writer.writerow(downstream_scores["cl"])
-
+        if args.experiment not in bound_experiments:
+            writer.writerow(sinedistance_scores["ae"])
+            writer.writerow(sinedistance_scores["cl"])
+        else:
+            writer.writerow(sinedistance_scores["cl"])
+            writer.writerow(sinedistance_upper_bounds["cl"])
+    if args.experiment not in bound_experiments:
+        with open(f"{args.ckptfldr}/{args.experiment}_downstream_score_{args.seed}.csv", "w+", newline='') as f:
+            writer = csv.writer(f)
+            if args.experiment == "increase_dimension_d":
+                writer.writerow(dimensions)
+            if args.experiment == "increase_dimension_r_model":
+                writer.writerow(dimensions)
+            if args.experiment == "increase_n":
+                writer.writerow(n_values)
+            if args.experiment == "increase_noise":
+                writer.writerow(noise_sigmas)
+            if args.experiment == "increase_noise_homogenous":
+                writer.writerow(noise_sigmas)
+            writer.writerow(downstream_scores["ae"])
+            writer.writerow(downstream_scores["cl"])
 
 if __name__ == "__main__":
     args = obtain_args()
-    # seeds = range(8, 10)
     seeds = range(10)
 
     for seed in seeds:
--- visualize_results.py
+++ visualize_results.py
@@ -4,9 +4,16 @@ import numpy as np
 
 def plot_graph(experiment, metric, seeds):
 
+    bound_experiments = ["bound_cl_with_d", "bound_cl_with_r", "bound_cl_with_n"]
+    bound_metrics = ["sinedistance"]
+    if experiment in bound_experiments:
+        if metric not in bound_metrics: return
+        test_bound = True    
+
     model_names = {'ae': 'Auto Encoders', 'cl': 'Contrastive Learning'}
     model_colors = {'ae': 'orange', 'cl': 'blue'}
     result_dict = {'ae': [], 'cl': []}
+    bound_dict = {'empirical': [], 'theoretical': []}
     for seed in seeds:
         rows = []
         with open(f"v0/{experiment}_{metric}_{seed}.csv") as f:
@@ -15,18 +22,34 @@ def plot_graph(experiment, metric, seeds):
             for row in csvreader:
                 rows.append(row)
 
-        result_dict[rows[0][0]].append([float(i) for i in rows[0][1:]])
-        result_dict[rows[1][0]].append([float(i) for i in rows[1][1:]])
+        if experiment not in bound_experiments:
+            result_dict[rows[0][0]].append([float(i) for i in rows[0][1:]])
+            result_dict[rows[1][0]].append([float(i) for i in rows[1][1:]])
+        if experiment in bound_experiments: 
+            bound_dict["empirical"].append([float(i) for i in rows[0][1:]])
+            bound_dict["theoretical"].append([float(i) for i in rows[1][1:]])
         xvals = [float(i) for i in header[1:]]
 
-    for model in result_dict:
-        all_results = np.array(result_dict[model])
+    if experiment not in bound_experiments:
+        for model in result_dict:
+            all_results = np.array(result_dict[model])
+            avg_results = np.mean(all_results, axis=0)
+            var_upper_bound = np.max(all_results, axis=0)
+            var_lower_bound = np.min(all_results, axis=0)
+
+            plt.plot(xvals, avg_results, label=model_names[model], color=model_colors[model], marker='o')
+            plt.fill_between(xvals, var_lower_bound, var_upper_bound, color=model_colors[model], alpha=0.3)
+    else:
+        all_results = np.array(bound_dict["empirical"])
         avg_results = np.mean(all_results, axis=0)
         var_upper_bound = np.max(all_results, axis=0)
         var_lower_bound = np.min(all_results, axis=0)
+        theoretical_upper_bound = np.mean(np.array(bound_dict["theoretical"]), axis=0) #Theoretical bound, will be same across all seeds
 
-        plt.plot(xvals, avg_results, label=model_names[model], color=model_colors[model], marker='o')
-        plt.fill_between(xvals, var_lower_bound, var_upper_bound, color=model_colors[model], alpha=0.3)
+        plt.plot(xvals, avg_results, label="Empirical sinedistance", color="blue", marker='o')
+        plt.fill_between(xvals, var_lower_bound, var_upper_bound, color="blue", alpha=0.3)
+        plt.plot(xvals, theoretical_upper_bound, label="Theoretical upper bound", color="orange", marker='+')
+
 
     plt.xlabel(header[0])
     plt.ylabel(metric)
@@ -42,7 +65,10 @@ if __name__ == "__main__":
     # experiment = "increase_dimension_d_and_r"
     # experiment = "increase_noise"
     # experiment = "increase_noise_homogenous"
-    experiment = "increase_dimension_r_model"
+    # experiment = "increase_dimension_r_model"
+    # experiment = "bound_cl_with_d"
+    experiment = "bound_cl_with_r"
+    # experiment = "bound_cl_with_n"
 
     metric = "sinedistance"
     plot_graph(experiment, metric, seeds)
