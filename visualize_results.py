7fd86aaa94fa83e3c5f838a51aca8850a690127a
Kshitij Singh <kanata@Kshitijs-MacBook-Pro.local>
2022-03-29 14:10:34 +0800

    Standardize Plots

--- visualize_results.py
+++ visualize_results.py
@@ -13,13 +13,20 @@ def plot_graph(experiment, metric, seeds):
     if experiment in cl_bound_experiments:
         if metric not in bound_metrics: return
 
-    model_names = {'ae': 'Auto Encoders', 'cl': 'Contrastive Learning'}
+    model_names = {'ae': 'Auto Encoder', 'cl': 'Contrastive Learning'}
     model_colors = {'ae': 'orange', 'cl': 'blue'}
+    metric_labels = {'sinedistance': "Sine Distance", 'downstream_score': "Downstream Score"}
+    value_labels = {'increase_dimension_d': "Input Dimension (d)", 'increase_dimension_r_model': "Latent Dimension of Model (r_model)", 'increase_n': "Number of Samples (N)", 
+                    'bound_cl_with_d': "Input Dimension (d)", 'bound_cl_with_r': "Latent Dimension (r)", 'bound_cl_with_n': "Number of Samples (N)",
+                    'increase_masking_ae': "Mask Percentage used in Augmentation", 'increase_noise': "Noise Sigma", 'increase_noise_homogenous': "(Homogeneous) Noise Sigma"}
     result_dict = {'ae': [], 'cl': []}
     bound_dict = {'empirical': [], 'theoretical': []}
+
     for seed in seeds:
         rows = []
-        with open(f"v0/{experiment}_{metric}_{seed}.csv") as f:
+        directory = "v0"
+        filepath = f'{directory}/{experiment}_{metric}_{seed}.csv'
+        with open(filepath) as f:
             csvreader = csv.reader(f)
             header = next(csvreader)
             for row in csvreader:
@@ -51,9 +58,9 @@ def plot_graph(experiment, metric, seeds):
         var_lower_bound = np.min(all_results, axis=0)
         theoretical_upper_bound = np.mean(np.array(bound_dict["theoretical"]), axis=0) #Theoretical bound, will be same across all seeds
 
-        plt.plot(xvals, avg_results, label="Empirical sinedistance", color="blue", marker='o')
+        plt.plot(xvals, avg_results, label="Empirical", color="blue", marker='o')
         plt.fill_between(xvals, var_lower_bound, var_upper_bound, color="blue", alpha=0.3)
-        plt.plot(xvals, theoretical_upper_bound, label="Theoretical upper bound", color="orange", marker='+')
+        plt.plot(xvals, theoretical_upper_bound, label="Upper Bound", color="orange", marker='+')
     elif experiment in ae_experiments:
         model="ae"
         all_results = np.array(result_dict["ae"])
@@ -64,10 +71,10 @@ def plot_graph(experiment, metric, seeds):
         plt.fill_between(xvals, var_lower_bound, var_upper_bound, color=model_colors[model], alpha=0.3)
 
 
-    plt.xlabel(header[0])
-    plt.ylabel(metric)
+    plt.xlabel(value_labels[experiment])
+    plt.ylabel(metric_labels[metric])
     plt.legend()
-    plt.savefig(f"{experiment}_{metric}.png")
+    plt.savefig(fname=f'{experiment}_{metric}.pdf', format="pdf", bbox_inches="tight", pad_inches=0.1)
     plt.clf()
 
 if __name__ == "__main__":
